8bb1804e8ddad3fc9f1526265091baa00e9a64e2
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2024-08-17 22:23:16 +0800

    RAG 目前无法实现大文本搜索

--- pm2-start-mac.sh
+++ pm2-start-mac.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-set -e
+# set -e
 
 root_path=$(cd "$(dirname "$0")" && pwd)
 cd ${root_path}
 
-# pm2 delete all
+pm2 delete ollama-11434
+pm2 delete Ollama-WebUI-8080
+pm2 delete ComfyUI-8188
+
+# git submodule update --init --recursive
+# sh scripts/ollama/build-mac.sh
+# sh scripts/open-webui/build-mac.sh
+# sh scripts/ComfyUI/build-mac.sh
 
 pm2 start ${root_path}/scripts/ollama/run-mac.sh --name ollama-11434
 pm2 start ${root_path}/scripts/open-webui/run-mac.sh --name Ollama-WebUI-8080
