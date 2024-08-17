8e38ba1fa405c20750f301224a5001a45682faa7
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2024-08-17 16:42:02 +0800

    优化编译脚本

--- scripts/open-webui/run-mac.sh
+++ scripts/open-webui/run-mac.sh
@@ -3,7 +3,7 @@ set -e
 
 root_path=$(cd "$(dirname "$0")" && pwd)
 folder_name=$(basename "$root_path")
-repo_path=${root_path}/../../embedded/${folder_name}
+repo_path=$(realpath "${root_path}/../../embedded/${folder_name}")
 cd ${repo_path}
 
 cd ./backend
