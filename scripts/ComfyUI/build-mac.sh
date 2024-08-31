810763b8d6a9079971820115e0488d3b3914ad3b
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2024-08-31 21:38:59 +0800

    ComfyUI-Manager 不限制安全级别

--- scripts/ComfyUI/build-mac.sh
+++ scripts/ComfyUI/build-mac.sh
@@ -19,4 +19,7 @@ pip install -r requirements.txt
 cd ${repo_path}/custom_nodes
 if [ ! -d "ComfyUI-Manager" ]; then
     git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+    # 本地运行，建议不限制安全级别
+    cd ComfyUI-Manager
+    sed -i '.bak' 's/^security_level = normal$/security_level = weak/' config.ini
 fi
